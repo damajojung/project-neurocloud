312a46c3603435fa49088a2cff631b36a06aeb95
damajojung <jungdamajo@gmail.com>
2022-12-21 17:34:41 +0100

    Saving

--- code/regression/regression_Streamlit.py
+++ code/regression/regression_Streamlit.py
@@ -5,13 +5,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-st.title("NBA Player Stats Explorer")
+st.title("Linear Regression")
 
 st.markdown(
     """
-This app performs simple webscraping of NBA player stats data!
-* **Python libraries:** base64, pandas, streamlit
-* **Data source:** [Basketball-reference.com](https://www.basketball-reference.com/).
+This app performs a linear regression from GDP per capita against the happiness index.
+* **Python libraries:** base64, pandas, streamlit, sci-kit learn
+* **Data source:** [satisfaction.com](https://stats.oecd.org/index.aspx?DataSetCode=BLI#).
+* **Data source:** [GDP.com](https://data.oecd.org/gdp/gross-domestic-product-gdp.htm).
 """
 )
 

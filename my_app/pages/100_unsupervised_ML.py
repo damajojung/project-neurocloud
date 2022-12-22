5dbb5b28204c381b0d67df625b41098323f6dd51
damajojung <jungdamajo@gmail.com>
2022-12-22 15:32:34 +0100

    saving

---
+++ my_app/pages/100_unsupervised_ML.py
@@ -0,0 +1,16 @@
+# This is the main page of the streamlit app.
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.set_page_config(layout="wide")
+
+st.title("Unsupervised Machine Learning")
+
+st.markdown(
+    """
+My resume.
+"""
+)
+

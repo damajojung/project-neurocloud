728eef1f5220c7c0cc05c5cb49286c1eb9152087
damajojung <jungdamajo@gmail.com>
2022-12-22 09:43:58 +0100

    saving

--- my_app/pages/page_2.py
+++ my_app/pages/page_2.py
@@ -4,6 +4,20 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+# Configure the appearance
+import matplotlib as mpl
+import seaborn as sns
+
+mpl.rc("axes", labelsize=5)
+mpl.rc("xtick", labelsize=5)
+mpl.rc("ytick", labelsize=5)
+
+st.set_page_config(layout="wide")
+
+plt.style.use(["ggplot"])
 
 st.title("Page 2 - Regression")
 
@@ -27,11 +41,24 @@ def load_data(path):
     return data
 
 
-data_load_state = st.text("Loading data...")
 dat = load_data(data_dir)
-data_load_state.text("Done! (using st.cache)")
 
 # Show data
 st.dataframe(dat)
 
+# Visualize the data
+X = np.array(dat.GDP)
+y = np.array(dat.Satisfaction)
+
+fig, ax = plt.subplots(1, figsize=(5, 3))
+ax.scatter(X, y, s=5)
+
+for i, txt in enumerate(dat.LOCATION):
+    ax.annotate(txt, (X[i], y[i]), size=4, c="black")
+
+ax.set_xlabel("GDP per capita (USD)")
+ax.set_ylabel("Life satisfaction")
+plt.show()
+
 # Display plot
+st.pyplot(fig)
--- my_app/streamlit_app.py
+++ my_app/streamlit_app.py
@@ -4,6 +4,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+st.set_page_config(layout="wide")
+
 st.title("Introduction")
 
 st.markdown(

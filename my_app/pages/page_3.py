c691688a1fc39d3dcfb45f1f6a8f4d0803ff3f09
damajojung <jungdamajo@gmail.com>
2022-12-22 07:53:15 +0100

    Saving

--- my_app/pages/page_3.py
+++ my_app/pages/page_3.py
@@ -1,2 +1,21 @@
 # This is the third page of the streamlit app.
 
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title("Page 3")
+
+st.markdown(
+    """
+This app performs a linear regression from GDP per capita against the happiness index.
+* **Python libraries:** base64, pandas, streamlit, sci-kit learn
+* **Data source:** [satisfaction.com](https://stats.oecd.org/index.aspx?DataSetCode=BLI#).
+* **Data source:** [GDP.com](https://data.oecd.org/gdp/gross-domestic-product-gdp.htm).
+"""
+)
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.line_chart(chart_data)
+

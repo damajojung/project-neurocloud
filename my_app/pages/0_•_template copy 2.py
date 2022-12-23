7d49c12dd774128058e70f09a83d8a2a700d4218
damajojung <jungdamajo@gmail.com>
2022-12-23 09:56:31 +0100

    Saving

---
+++ my_app/pages/0_•_template copy 2.py
@@ -0,0 +1,16 @@
+# This is the main page of the streamlit app.
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.set_page_config(layout="wide")
+
+st.title("Resume")
+
+st.markdown(
+    """
+My resume.
+"""
+)
+

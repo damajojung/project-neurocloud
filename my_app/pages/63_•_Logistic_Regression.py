54ddcab90c84dc5e7bbef5ff28f37b0640a0cfbe
damajojung <jungdamajo@gmail.com>
2022-12-27 17:28:06 +0100

    saving

--- my_app/pages/63_•_Logistic_Regression.py
+++ my_app/pages/63_•_Logistic_Regression.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
+from sklearn.linear_model import LinearRegression
+from sklearn import datasets
 
 st.set_page_config(layout="wide")
 
@@ -11,4 +16,89 @@ st.markdown(
 Logistic Regression.
 """
 )
+# Functions
+# Creating function for getting plots
+def get_figure(x, y):
 
+    fig, ax = plt.subplots(1, figsize=(10, 6))
+    ax.scatter(x, y, s=7)
+
+    ax.set_xlabel("GDP per capita (USD)")
+    ax.set_ylabel("Life satisfaction")
+    return fig, ax
+
+
+# Data
+@st.cache
+def load_data():
+    data = datasets.load_iris()
+    return data
+
+
+iris = load_data()
+
+### Machine Learning
+
+# Let’s try to build a classifier to detect the Iris virginica type based only on the petal width feature. First let’s load the data:
+
+X = iris["data"][:, 3:]  # 3: petal width
+y = (iris["target"] == 2).astype(int)  # 1 if Iris virginica, else 0
+
+from sklearn.linear_model import LogisticRegression
+
+log_reg = LogisticRegression(solver="lbfgs", random_state=42)
+log_reg.fit(X, y)
+
+# Plot the result
+X_new = np.linspace(0, 3, 1000).reshape(-1, 1)
+y_proba = log_reg.predict_proba(X_new)
+decision_boundary = X_new[y_proba[:, 1] >= 0.5][0]
+
+fig, ax = get_figure(X, y)
+plt.plot(X[y == 0], y[y == 0], "bs")
+plt.plot(X[y == 1], y[y == 1], "g^")
+plt.plot([decision_boundary, decision_boundary], [-1, 2], "r:", linewidth=2)
+plt.plot(X_new, y_proba[:, 1], "g-", linewidth=2, label="Iris virginica")
+plt.plot(X_new, y_proba[:, 0], "b--", linewidth=2, label="Not Iris virginica")
+plt.xlabel("Petal width (cm)", fontsize=14)
+plt.ylabel("Probability", fontsize=14)
+plt.legend(loc="center left", fontsize=14)
+plt.axis([0, 3, -0.02, 1.02])
+st.pyplot(fig)
+
+
+##### Two Features - Softmax Regression
+
+X = iris["data"][:, (2, 3)]  # petal length, petal width
+y = (iris["target"] == 2).astype(int)
+log_reg = LogisticRegression(solver="lbfgs", C=10 ** 10, random_state=42)
+log_reg.fit(X, y)
+
+x0, x1 = np.meshgrid(
+    np.linspace(2.9, 7, 500).reshape(-1, 1), np.linspace(0.8, 2.7, 200).reshape(-1, 1),
+)
+X_new = np.c_[x0.ravel(), x1.ravel()]
+
+y_proba = log_reg.predict_proba(X_new)
+
+fig, ax = get_figure(X[:, 0], X[:, 1])
+plt.plot(X[y == 0, 0], X[y == 0, 1], "bs")
+plt.plot(X[y == 1, 0], X[y == 1, 1], "g^")
+
+zz = y_proba[:, 1].reshape(x0.shape)
+contour = plt.contour(x0, x1, zz, cmap=plt.cm.brg)
+
+
+left_right = np.array([2.9, 7])
+boundary = (
+    -(log_reg.coef_[0][0] * left_right + log_reg.intercept_[0]) / log_reg.coef_[0][1]
+)
+
+plt.clabel(contour, inline=1, fontsize=12)
+plt.plot(left_right, boundary, "k--", linewidth=3)
+plt.text(3.5, 1.5, "Not Iris virginica", fontsize=14, color="b", ha="center")
+plt.text(6.5, 2.3, "Iris virginica", fontsize=14, color="g", ha="center")
+plt.xlabel("Petal length", fontsize=14)
+plt.ylabel("Petal width", fontsize=14)
+plt.axis([2.9, 7, 0.8, 2.7])
+st.pyplot(fig)

acde20715d62c776bc6b5073ec3cfcf81fb29d2c
damajojung <jungdamajo@gmail.com>
2022-12-29 09:02:54 +0100

    saving

--- my_app/pages/101_•_K-Means.py
+++ my_app/pages/101_•_K-Means.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+import seaborn as sns
+import sklearn
+from sklearn import datasets
+import os
+from sklearn.datasets import make_blobs
 
 st.set_page_config(layout="wide")
 
@@ -11,4 +18,136 @@ st.markdown(
 K-Means Cluster.
 """
 )
+
+blob_centers = np.array(
+    [[-0.5, -0.5], [-1.5, 2.5], [-3.0, 0.5], [-3.3, 2.8], [-2.7, -1.0]]
+)
+blob_std = np.array([0.3, 0.3, 0.15, 0.3, 0.2])
+
+X, y = make_blobs(
+    n_samples=2000, centers=blob_centers, cluster_std=blob_std, random_state=7
+)
+
+
+def plot_clusters(X, y=None):
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=1)
+    plt.xlabel("$x_1$", fontsize=14)
+    plt.ylabel("$x_2$", fontsize=14, rotation=0)
+
+
+fig = plt.figure(figsize=(8, 4))
+plot_clusters(X)
+st.pyplot(fig)
+
+######
+
+from sklearn.cluster import KMeans
+
+k = 5
+kmeans = KMeans(n_clusters=k, random_state=42)
+y_pred = kmeans.fit_predict(X)
+
+st.caption(y_pred)
+st.caption(y_pred is kmeans.labels_)
+
+# The following 5 centroid were calculated:
+kmeans.cluster_centers_
+
+kmeans.labels_
+
+# Predict the labels for new instances
+X_new = np.array([[0, 2], [3, 2], [-3, 3], [-3, 2.5]])
+kmeans.predict(X_new)
+
+
+def plot_data(X):
+    plt.plot(X[:, 0], X[:, 1], "k.", markersize=2)
+
+
+def plot_centroids(centroids, weights=None, circle_color="w", cross_color="k"):
+    if weights is not None:
+        centroids = centroids[weights > weights.max() / 10]
+    plt.scatter(
+        centroids[:, 0],
+        centroids[:, 1],
+        marker="o",
+        s=35,
+        linewidths=8,
+        color=circle_color,
+        zorder=10,
+        alpha=0.9,
+    )
+    plt.scatter(
+        centroids[:, 0],
+        centroids[:, 1],
+        marker="x",
+        s=2,
+        linewidths=12,
+        color=cross_color,
+        zorder=11,
+        alpha=1,
+    )
+
+
+def plot_decision_boundaries(
+    clusterer,
+    X,
+    resolution=1000,
+    show_centroids=True,
+    show_xlabels=True,
+    show_ylabels=True,
+):
+    mins = X.min(axis=0) - 0.1
+    maxs = X.max(axis=0) + 0.1
+    xx, yy = np.meshgrid(
+        np.linspace(mins[0], maxs[0], resolution),
+        np.linspace(mins[1], maxs[1], resolution),
+    )
+    Z = clusterer.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    plt.contourf(Z, extent=(mins[0], maxs[0], mins[1], maxs[1]), cmap="Pastel1")
+    plt.contour(
+        Z, extent=(mins[0], maxs[0], mins[1], maxs[1]), linewidths=1, colors="k"
+    )
+    plot_data(X)
+    if show_centroids:
+        plot_centroids(clusterer.cluster_centers_)
+
+    if show_xlabels:
+        plt.xlabel("$x_1$", fontsize=14)
+    else:
+        plt.tick_params(labelbottom=False)
+    if show_ylabels:
+        plt.ylabel("$x_2$", fontsize=14, rotation=0)
+    else:
+        plt.tick_params(labelleft=False)
+
+
+fig = plt.figure(figsize=(8, 4))
+plot_decision_boundaries(kmeans, X)
+st.pyplot(fig)
+
+
+###
+
+st.title("Finding the right k (amount of clusters)")
+
+kmeans_per_k = [KMeans(n_clusters=k, random_state=42).fit(X) for k in range(1, 10)]
+inertias = [model.inertia_ for model in kmeans_per_k]
+
+fig = plt.figure(figsize=(8, 3.5))
+plt.plot(range(1, 10), inertias, "bo-")
+plt.xlabel("$k$", fontsize=14)
+plt.ylabel("Inertia", fontsize=14)
+plt.annotate(
+    "Elbow",
+    xy=(5, inertias[4]),
+    xytext=(0.55, 0.55),
+    textcoords="figure fraction",
+    fontsize=16,
+    arrowprops=dict(facecolor="black", shrink=0.1),
+)
+plt.axis([1, 8.5, 0, 1300])
+st.pyplot(fig)
 

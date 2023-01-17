ade74d394b64d6b407cba3b7e6f39b097bd1a820
damajojung <jungdamajo@gmail.com>
2023-01-17 07:52:47 +0100

    saving

--- my_app/pages/41_•_Multiple_Linear_Regression.py
+++ my_app/pages/41_•_Multiple_Linear_Regression.py
@@ -5,10 +5,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import sklearn
 from sklearn import linear_model
+import statsmodels.api as sm
+from statsmodels.stats.outliers_influence import variance_inflation_factor
 
 st.set_page_config(layout="wide")
 
 st.title("Multiple Linear Regression")
+
+st.title("Check out how the numbers get better regarding R2.")
 
 st.markdown(
     r"""
@@ -25,7 +29,7 @@ st.markdown(
     r"""
 Let us assume that there is a certain bush in California which does not like hot weather. Therefore, it can be found in the mountains 
 where the temperatures are lower thoughout the year. Scientist have collected data about the sea level, the height of the bush, the 
-size of the leafs and the concentration of magnesium and caesium in the soil 1 meter apart from the roots. The data can be seen in the following data frame.
+size of the leafs and the concentration of magnesium and caesium in the soil 1 meter from the roots. The data can be seen in the following data frame.
 """
 )
 
@@ -43,8 +47,10 @@ mpl.rc("ytick", labelsize=10)
 m = 0.008
 c = 6  # data with approximate straight line
 x = np.linspace(50, 2000, 100)
-y = m * x + c + np.random.randn(100) * 2 + 150
-z = m * x + c + np.random.randn(100) * 1 + 10
+y = (
+    m * x + c + np.random.randn(100) * 1 + 150
+)  # I guess I can change quite a bit in here.
+z = m * x + c + np.random.randn(100) * 1 + 10  # prediction
 
 # Bad data
 y_1 = m * x + c + np.random.randn(100) * 10  # looks random
@@ -82,8 +88,10 @@ st.pyplot(fig)
 
 st.markdown(
     r"""
-Since we deal only with 3 variables, we can plot this data nicely. However, if you are dealing with more than three values, one has to use a visualisation
-and check, which vairables make sense to take into consideration. This can be seen in the following plot. 
+If we were dealing with only 3 variables, let's say the first three, we can plot this data nicely. However, if you are dealing with more than three values, 
+one has to use a visualisation
+and check, which variables make sense to take into consideration. Moreover, the data in 3D-Plots can sometimes be a bit tricky to grasp. Therefore,
+it is advisable to stick to 2D-plots which can be seen in the following figure where each variable of the data set is plotted against another one. 
 """
 )
 
@@ -97,11 +105,58 @@ st.pyplot(fig)
 
 st.markdown(
     r"""
-It is clearly visible that all the variables share a linear relationship. Therefore, it makes sense to use all of them. However, if you are dealing with hundrets
-of variables, one has to use a faster way to determine which variables are worthwhile using and which are not. 
+It is clearly visible that Sealevel, Height and Leaf Size share a linear relationship. Therefore, it makes sense to use them. Magnesium seems to be more of a
+random nature and caesium has clearly not a linear relationship with most other variables. As we can see, this is a fast way one can 
+visualise and check the relationships of several variables. However, if you are dealing with hundrets
+of variables, one has to use a faster way to determine which variables are worthwhile using and which ones are not. 
 """
 )
-# Prediction
+# Prediction FULL
+
+X_full = pd.DataFrame(
+    {"sealevel": x, "height": y, "Magnesium": y_1, "Caesium": y_2}
+).values.reshape(-1, 4)
+
+# add constant to predictor variables
+x_full = sm.add_constant(X_full)
+
+# fit linear regression model
+model = sm.OLS(z, x_full).fit()
+
+# view model summary
+summary = model.summary()
+
+st.text(f"{summary}")
+
+st.markdown(
+    r"""
+The p-values of each variable must be < .05. Therefore, Sealevel and Height of the bush are worthwhile keeping in the model. The concantration of magnesium and caesium not. 
+Let's remove them and check the statistics again.
+"""
+)
+
+#################################### Getting statistics
+
+X = pd.DataFrame({"sealevel": x, "height": y}).values.reshape(-1, 2)
+
+# add constant to predictor variables
+x_final = sm.add_constant(X)
+
+# fit linear regression model
+model = sm.OLS(z, x_final).fit()
+
+# view model summary
+summary = model.summary()
+
+st.markdown(
+    r"""
+The p-values of each variable must be < .05. Therefore, both variables work in this case and we can do a 3D-plot for ilustration puposes. 
+"""
+)
+
+st.text(f"{summary}")
+
+# Only 3 variables
 
 ols = linear_model.LinearRegression()
 model = ols.fit(X, z)
@@ -110,7 +165,7 @@ predicted = model.predict(model_viz)
 
 r2 = model.score(X, z)
 
-### Show data with prediction surface
+### Show data with prediction surface - final plot
 
 fig1 = plt.figure(figsize=(15, 5))
 
@@ -144,31 +199,9 @@ ax3.view_init(elev=60, azim=165)
 
 st.pyplot(fig1)
 
-#################################### Getting statistics
-
-import statsmodels.api as sm
-
-# add constant to predictor variables
-x = sm.add_constant(X)
-
-# fit linear regression model
-model = sm.OLS(y, X).fit()
-
-# view model summary
-summary = model.summary()
-
-st.markdown(
-    r"""
-The p-values of each variable must be < .05. Therefore, both vairables work in this case. 
-"""
-)
-
-st.text(f"{summary}")
-
 ################################### Getting VIF
 
 # Checking for the VIF values of the variables.
-from statsmodels.stats.outliers_influence import variance_inflation_factor
 
 # Creating a dataframe that will contain the names of all the feature variables and their VIFs
 vif = pd.DataFrame()
